468ae5586f29ff5ef2bb310db447053d0bb9b32c
Luis Lopez <luis.lopez@altoros.com>
2022-09-07 14:17:09 -0300

    Very basic call handler, test Vault creation

--- tests/controller.test.ts
+++ tests/controller.test.ts
@@ -3,80 +3,51 @@ import {
   describe,
   test,
   clearStore,
-  beforeAll,
-  afterAll
-} from "matchstick-as/assembly/index"
-import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { SharePriceChangeLog } from "../generated/Controller/Controller"
-import { handleSharePriceChangeLog } from "../src/controller"
-import { createSharePriceChangeLogEvent } from "./controller-utils"
+  afterAll,
+  newMockCall,
+} from "matchstick-as/assembly/index";
+import { AddVaultAndStrategyCall } from "../generated/Controller/Controller";
+import { Address, ethereum } from "@graphprotocol/graph-ts";
+import { handleAddVaultAndStrategy } from "../src/controller";
 
-// Tests structure (matchstick-as >=0.5.0)
-// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+function mockCall(vault: Address, strategy: Address): AddVaultAndStrategyCall {
+  let to = Address.fromString("0x222412af183bceadefd72e4cb1b71f1889953b1c");
+  let from = Address.fromString("0x0000000000000000000000000000000000000001");
+  let call = newMockCall();
+  call.to = to;
+  call.from = from;
+  call.inputValues = [
+    new ethereum.EventParam("vault", ethereum.Value.fromAddress(vault)),
+    new ethereum.EventParam("strategy", ethereum.Value.fromAddress(strategy)),
+  ];
 
-describe("Describe entity assertions", () => {
-  beforeAll(() => {
-    let vault = Address.fromString("0x0000000000000000000000000000000000000001")
-    let strategy = Address.fromString(
-      "0x0000000000000000000000000000000000000001"
-    )
-    let oldSharePrice = BigInt.fromI32(234)
-    let newSharePrice = BigInt.fromI32(234)
-    let timestamp = BigInt.fromI32(234)
-    let newSharePriceChangeLogEvent = createSharePriceChangeLogEvent(
-      vault,
-      strategy,
-      oldSharePrice,
-      newSharePrice,
-      timestamp
-    )
-    handleSharePriceChangeLog(newSharePriceChangeLogEvent)
-  })
+  return changetype<AddVaultAndStrategyCall>(call);
+}
 
+describe("Controller", () => {
   afterAll(() => {
-    clearStore()
-  })
+    clearStore();
+  });
 
-  // For more test scenarios, see:
-  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+  describe("addVaultAndStrategy", () => {
+    test("creates Vault", () => {
+      let vaultAddress = Address.fromString(
+        "0x0000000000000000000000000000000000000002"
+      );
+      let strategyAddress = Address.fromString(
+        "0x0000000000000000000000000000000000000003"
+      );
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+      let call = mockCall(vaultAddress, strategyAddress);
 
-    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "vault",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "strategy",
-      "0x0000000000000000000000000000000000000001"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "oldSharePrice",
-      "234"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "newSharePrice",
-      "234"
-    )
-    assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "timestamp",
-      "234"
-    )
+      handleAddVaultAndStrategy(call);
 
-    // More assert options:
-    // https://thegraph.com/docs/en/developer/matchstick/#asserts
-  })
-})
+      assert.fieldEquals(
+        "Vault",
+        vaultAddress.toHexString(),
+        "id",
+        vaultAddress.toHexString()
+      );
+    });
+  });
+});
