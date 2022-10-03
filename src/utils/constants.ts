347b3a3ff6d1afff5640c981817a9634934c2eb5
0xLucca <0xlucca.dev@gmail.com>
2022-10-03 13:20:01 -0300

    fix: constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,11 +7,12 @@ export namespace constants {
   export const SECONDS_PER_DAY = 60 * 60 * 24;
   export const MAX_BPS = BigInt.fromI32(10000);
   export const DEFAULT_DECIMALS = BigInt.fromI32(18);
-  export const PROTOCOL_ID = "";
 
   export const CONTROLLER_ADDRESS = Address.fromString(
     "0x222412af183bceadefd72e4cb1b71f1889953b1c"
   );
+
+  export const PROTOCOL_ID = CONTROLLER_ADDRESS;
 
   export const CHAIN_LINK_CONTRACT_ADDRESS = Address.fromString(
     "0x47fb2585d2c56fe188d0e6ec628a38b74fceeedf"
